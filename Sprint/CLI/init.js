2049e3d4b0732bf2507e5cc79be60540b25a3577
Michael O'Reilly <86013664+MichaelOrSd@users.noreply.github.com>
2022-02-22 16:17:30 -0330

    init

--- Sprint/CLI/init.js
+++ Sprint/CLI/init.js
@@ -13,6 +13,7 @@ myapp init --cat          creates the config file with default settings`;
 
 function initializeApp() {
   const myArgs = process.argv.slice(2);
+
   switch (myArgs[1]) {
     case "--all":
       if (DEBUG) console.log("initializeApp.All() --all");
@@ -30,13 +31,18 @@ function initializeApp() {
   }
 }
 
-if (fs.existsSync(path.join(__dirname, "./views"))) {
-  fs.writeFile("./views/init.txt", init, (err) => {
-    if (err) console.log(err);
-    else console.log("Data written to init.txt file");
-  });
-} else {
-  fsPromises.mkdir(path.join(__dirname, "views"));
+function createInit() {
+  if (fs.existsSync(path.join(__dirname, "./views"))) {
+    fs.writeFile(path.join(__dirname, "views", "init.txt"), init, (err) => {
+      if (err) console.log(err);
+      else if (DEBUG) console.log("Data written to init.txt file.");
+    });
+  } else {
+    fs.mkdir(path.join(__dirname, "./views"), (err) => {
+      if (err) console.log(err);
+      else if (DEBUG) console.log("Directory created.");
+    });
+  }
 }
 
 const config = {
@@ -47,18 +53,51 @@ const config = {
   superuser: "adm1n",
 };
 
-try {
-  let data = JSON.stringify(config, null, 2);
-  if (!fs.existsSync(path.join(__dirname, "config.json"))) {
-    fs.writeFile("config.json", data, (err) => {
-      console.log("Data written to config.json file");
-    });
-  } else {
-    console.log("config.json file already exists");
+function createConfig() {
+  try {
+    let data = json.stringify(config, null, 2);
+    if (!fs.existsSync(path.join(__dirname, "config.json"))) {
+      fs.writeFile("config.json", data, (err) => {
+        if (DEBUG) console.log("Data written to config.json file.");
+      });
+    } else {
+      if (DEBUG) console.log("config.json file already exists.");
+    }
+  } catch (err) {
+    console.log(err);
   }
-} catch (err) {
-  console.log(err);
 }
+
+// below code works
+// if (fs.existsSync(path.join(__dirname, "./views"))) {
+//   fs.writeFile("./views/init.txt", init, (err) => {
+//     if (err) console.log(err);
+//     else console.log("Data written to init.txt file");
+//   });
+// } else {
+//   fsPromises.mkdir(path.join(__dirname, "views"));
+// }
+
+// const config = {
+//   name: "AppConfigCLI",
+//   version: "1.0.0",
+//   description: "The Command Line Interface (CLI) for the MyApp.",
+//   main: "myapp.js",
+//   superuser: "adm1n",
+// };
+
+// try {
+//   let data = JSON.stringify(config, null, 2);
+//   if (!fs.existsSync(path.join(__dirname, "config.json"))) {
+//     fs.writeFile("config.json", data, (err) => {
+//       console.log("Data written to config.json file");
+//     });
+//   } else {
+//     console.log("config.json file already exists");
+//   }
+// } catch (err) {
+//   console.log(err);
+// }
 
 module.exports = {
   init: init,
