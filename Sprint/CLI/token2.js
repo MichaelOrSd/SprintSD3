8e0c431a21d0bf01692935b8b666c1ed247bd2be
Michael O'Reilly <86013664+MichaelOrSd@users.noreply.github.com>
2022-02-24 11:25:51 -0330

    init

--- Sprint/CLI/token2.js
+++ Sprint/CLI/token2.js
@@ -104,3 +104,36 @@ function newToken(username) {
   });
   return newToken.token;
 }
+
+function updateToken(argv) {
+    if(DEBUG) console.log('token.updateToken()');
+    if(DEBUG) console.log(argv);
+    fs.readFile(__dirname + '/json/tokens.json', 'utf8', (error, data) => {
+        if(error) throw error;
+        let tokens = JSON.parse(data);
+        tokens.forEach((obj) => {
+            if(obj.username === argv[3]) {
+                if(DEBUG) console.log(obj);
+                switch(argv[2]) {
+                    case 'p':
+                    case 'P':
+                        obj.phone = argv[4];
+                        break;
+                    case 'e':
+                    case 'E':
+                        obj.email = argv[4];
+                        break;
+                    default:
+                }
+                if(DEBUG) console.log(obj);
+            }
+        ));
+        userTokens = JSON.stringify(tokens);
+        fs.watchFile(__dirname + '/json/tokens.json', userTokens, (err) => {
+            if(err) console.log(err);
+            else {
+                console.log(`Token record for ${argv[3]} was updated with ${argv[4]}.`);
+                myEmitter.emit('log', 'tokens.updateToken()', 'INFO', `Token ${argv[3]} was updated.`);
+            }
+        })
+}
