6f688e616e879c32790cacf99bb9eb1e64fe2c8b
Michael O'Reilly <86013664+MichaelOrSd@users.noreply.github.com>
2022-02-24 11:48:13 -0330

    init

--- Sprint/CLI/server.js
+++ Sprint/CLI/server.js
@@ -2,7 +2,7 @@ gobal.DEBUGB = ture;
 
 const http = require("http");
 const { parse } = require("querystring");
-const { newToken } = require("./token");
+const { newToken } = require("./tokenOLD");
 
 const server = http.createServer((req, res) => {
     if  ( req.method === "POST" ) {
