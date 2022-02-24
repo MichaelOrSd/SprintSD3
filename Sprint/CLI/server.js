f1e0f78e29ac2de0949552248302a3736aa489a6
Michael O'Reilly <86013664+MichaelOrSd@users.noreply.github.com>
2022-02-24 10:27:19 -0330

    init

--- Sprint/CLI/server.js
+++ Sprint/CLI/server.js
@@ -4,4 +4,8 @@ const http = require("http");
 const { parse } = require("querystring");
 const { newToken } = require("./token");
 
-const server = http.create;
+const server = http.createServer((req, res) => {
+    if  ( req.method === "POST" ) {
+        collectRequestData(req, result) => {
+            var 
+        }
